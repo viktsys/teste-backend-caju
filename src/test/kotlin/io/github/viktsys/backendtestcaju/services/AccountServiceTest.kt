f8586bc7feb0059be636e7264347841ee7017f5e
Carlos Victor M. da Silva viktsys <viktsys@gmail.com>
2024-08-01 21:11:40 -0300

    Fixing wrong settings configurations.

--- src/test/kotlin/io/github/viktsys/backendtestcaju/services/AccountServiceTest.kt
+++ src/test/kotlin/io/github/viktsys/backendtestcaju/services/AccountServiceTest.kt
@@ -7,11 +7,13 @@ import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import kotlin.test.assertNotNull
 
 @ExtendWith(SpringExtension::class)
 @SpringBootTest
+@ActiveProfiles("test")
 @AutoConfigureMockMvc
 class AccountServiceTest {
 
--- src/test/kotlin/io/github/viktsys/backendtestcaju/services/TransactionServiceTest.kt
+++ src/test/kotlin/io/github/viktsys/backendtestcaju/services/TransactionServiceTest.kt
@@ -11,11 +11,13 @@ import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import kotlin.test.assertEquals
 
 @ExtendWith(SpringExtension::class)
 @SpringBootTest
+@ActiveProfiles("test")
 @AutoConfigureMockMvc
 class TransactionServiceTest {
 
